fcb5779cbb631345c44bb7dd06bd71e1f4aff696
pranavbelorkar <168989489+pranavbelorkar@users.noreply.github.com>
2024-12-04 14:25:18 +0530

    Update Signup.jsx

--- frontend/src/Component/Signup.jsx
+++ frontend/src/Component/Signup.jsx
@@ -31,7 +31,7 @@ export function Signup() {
     setError('');
 
     try {
-      const response = await fetch('/api/users/signup', {
+      const response = await fetch('https://gaurav-cars.vercel.app/api/users/signup', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
